d2c0eeb3f8078c3d2a3991580bbd671bb4fb1199
FrancieleAires <f.aires2016@gmail.com>
2023-04-06 21:16:48 -0300

    Adicionar arquivos de projeto.

---
+++ Matriz1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace Matriz1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, IdadeMaior = 0, PosicaoIdadeMaior = 0;
+
+            Console.WriteLine("Digite um número: ");
+            N = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite um nome e uma idade: ");
+            string[] nomes = new string[N];
+            int [] idades = new int[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                string [] s = Console.ReadLine().Split(' ');
+                nomes[i] = s[0];
+                idades[i] = int.Parse(s[1]);
+            }
+            for(int i = 0; i < N; i++)
+            {
+                if (idades[i] > IdadeMaior)
+                {
+                    IdadeMaior = idades[i];
+                    PosicaoIdadeMaior = i;
+                } 
+            }
+
+            Console.WriteLine("Pessoa mais velha: " + nomes[PosicaoIdadeMaior]);
+
+        }
+    }
+}
